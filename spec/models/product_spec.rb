d5e0a14a2e448d443c3c34d17886e58347747ba4
Pedro Lorandi <hello@pedrolorandi.com>
2023-04-25 10:53:12 -0400

    Add model specs for the Product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    before(:each) do
+      @category = Category.create(name: 'Test Category')
+      @product = Product.new(name: 'Test Product', price: 100, quantity: 5, category: @category)
+    end
+
+    it 'should save successfully with all four fields set' do
+      expect(@product.save).to be true
+    end
+
+    it 'should not save without a name' do
+      @product.name = nil
+      expect(@product.save).to be false
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'should not save without a price' do
+      @product.price_cents = nil
+      expect(@product.save).to be false
+      expect(@product.errors.full_messages).to include("Price cents is not a number", "Price is not a number", "Price can't be blank")
+    end
+
+    it 'should not save without a quantity' do
+      @product.quantity = nil
+      expect(@product.save).to be false
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'should not save without a category' do
+      @product.category = nil
+      expect(@product.save).to be false
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+  end
+end
